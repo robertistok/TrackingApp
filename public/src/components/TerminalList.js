a6c9580c40c61b48df464af4f57066083aaa0d05
robertistok <robertistok94@gmail.com>
2016-12-22 18:42:42 +0200

    Add a button, when clicked, it updates the selected data

--- public/src/components/TerminalList.js
+++ public/src/components/TerminalList.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default class TerminalList extends React.Component {
   render() {
-    const { fetchTerminals, terminalsList } = this.props;
+    const { fetchTerminals, terminalsList, currentID, handleChange } = this.props;
     return(
       <div className="form-group">
         <label htmlFor='terminalID'>Select a terminal</label>
-        <select className="form-control" onChange={fetchTerminals} id='terminalID' defaultValue="def">
+        <select className="form-control" onChange={handleChange} id='terminalID' defaultValue="def">
           <option value="def" disabled>Select a device</option>
           {
             terminalsList.terminals.map(function(terminal) {
@@ -14,6 +14,7 @@ export default class TerminalList extends React.Component {
             })
           }
         </select>
+        <button onClick={handleChange} className="btn" value={currentID}>Update positions</button>
       </div>
     )
   }
--- public/src/containers/TerminalListContainer.js
+++ public/src/containers/TerminalListContainer.js
@@ -3,17 +3,44 @@ import { connect } from 'react-redux';
 import TerminalList from '../components/TerminalList';
 import { fetchSelectedsPositions, fetchSelectedsPositionsError, fetchSelectedsPositionsSucces } from '../actions/form';
 
-const mapStateToProps = (state) => {
-  return {
-    terminalsList: state.devices.terminalsList
+class TerminalListContainer extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.value)
+    let id = event.target.value;
+    this.props.fetchTerminals(id);
+  }
+  render() {
+    return (
+      <TerminalList {...this.props} handleChange={this.handleChange}/>
+    )
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
+const mapStateToProps = (state) => {
   return {
-    fetchTerminals: (event) => {
-      let id = event.target.value;
+    terminalsList: state.devices.terminalsList,
+    currentID: state.devices.selectedTerminal.id
+  }
+}
+
+const mapDispatchToProps = (dispatch, state) => {
+  return {
+    fetchTerminals: (id) => {
       dispatch(fetchSelectedsPositions(id)).then((response) => {
+        console.log(response);
+        if(response.error) dispatch(fetchSelectedsPositionsError(response.payload));
+        dispatch(fetchSelectedsPositionsSucces(response.payload))
+      });
+    },
+
+    updateTerminals: (id) => {
+      dispatch(fetchSelectedsPositions(id)).then((response) => {
+        console.log(response);
         if(response.error) dispatch(fetchSelectedsPositionsError(response.payload));
         dispatch(fetchSelectedsPositionsSucces(response.payload))
       });
@@ -21,4 +48,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(TerminalList);
+export default connect(mapStateToProps, mapDispatchToProps)(TerminalListContainer);
